feb200c7f94e10c744c856f51e10ab0fd54a07f0
Mir Riaz Ali <60673748+riazzx@users.noreply.github.com>
2022-11-15 21:49:11 +0400

    [ADD] Frontend for homepage created in index.html

---
+++ lessons.js
@@ -0,0 +1,84 @@
+lessons = [
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Science",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "science.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    },
+    {
+        id: 1000,
+        title: "Math",
+        description: "Cool Subject bro",
+        price: 1000,
+        image: "calculator.png",
+        availableLessons: 10
+    }
+   
+
+]
