87da1b8555d7471d9c985a98ad8d9220a0df9572
Mir Riaz Ali <60673748+riazzx@users.noreply.github.com>
2023-02-07 10:19:01 +0400

    Revert "initialize"
    This reverts commit 0981442baf6e4c5e547b41beff266e13f07256a3.

---
+++ lessons.js
@@ -0,0 +1,104 @@
+lessons = [
+    {
+        id: 1001,
+        title: "General Knowledge",
+        description: "Learn interesting fact about the world, sports, politics, and more!",
+        price: 100 ,
+        location: "Dubai",
+        image: "assets/idea.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1002,
+        title: "Science",
+        description: "Learn exciting science experiments and know the reason behind them!",
+        price: 200,
+        location: "Abu Dhabi",
+        image: "assets/science.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1003,
+        title: "Coding",
+        description: "Develop cool software using the latest trending technologies!",
+        price: 400,
+        location: "Sharjah",
+        image: "assets/coding.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1004,
+        title: "Cooking",
+        description: "Learn to prepare your favorite recipes!",
+        price: 300,
+        location: "Dubai",
+        image: "assets/cooking.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1005,
+        title: "Knitting",
+        description: "Create artistic garments learning to knit!",
+        price: 650,
+        location: "Abu Dhabi",
+        image: "assets/knitting.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1006,
+        title: "Pottery",
+        description: "Experience the ancient method of using clay to make pots!",
+        price: 700,
+        location: "Sharjah",
+        image: "assets/pottery.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1007,
+        title: "Singing",
+        description: "Learn to sing from the best vocal teachers in the city!",
+        price: 600,
+        location: "Fujairah",
+        image: "assets/singing.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1008,
+        title: "Dancing",
+        description: "Learn some sick moves to impress your crush!",
+        price: 800,
+        location: "Dubai",
+        image: "assets/dance.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1009,
+        title: "Acting",
+        description: "Learn practical techniques for analyzing film and theatre scripts!",
+        price: 900,
+        location: "Fujairah",
+        image: "assets/clapperboard.png",
+        totalLessons: 10,
+        availableLessons: 5
+    },
+    {
+        id: 1010,
+        title: "Music",
+        description: "Learn to play your favorite instrument!",
+        price: 1000,
+        location: "Sharjah",
+        image: "assets/musical-note.png",
+        totalLessons: 10,
+        availableLessons: 5
+    }
+   
+
+]
