bfc12edc87711a794b4d2ab253c5bac599fa86d4
Mir Riaz Ali <60673748+riazzx@users.noreply.github.com>
2023-02-07 10:11:36 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -22,5 +22,16 @@ self.addEventListener('install', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
-    console.log('[Service Worker] Fetch resource' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then( function (r) {
+            console.log('[Service Worker] Fetch resource' + e.request.url)
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone())
+                    return response
+                })
+            })
+        })
+    )
+    
 })
