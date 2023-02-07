421d2a067c6088c0954a2b0722653040710d8cc9
Mir Riaz Ali <60673748+riazzx@users.noreply.github.com>
2023-02-07 10:18:54 +0400

    Revert "Update service-worker.js"
    This reverts commit bfc12edc87711a794b4d2ab253c5bac599fa86d4.

--- service-worker.js
+++ service-worker.js
@@ -22,16 +22,5 @@ self.addEventListener('install', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then( function (r) {
-            console.log('[Service Worker] Fetch resource' + e.request.url)
-            return r || fetch(e.request).then(function (response) {
-                return caches.open(cacheName).then(function (cache) {
-                    cache.put(e.request, response.clone())
-                    return response
-                })
-            })
-        })
-    )
-    
+    console.log('[Service Worker] Fetch resource' + e.request.url)
 })
